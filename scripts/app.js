650d5e2fdaeddf783f220870fb21119db4027eb1
LeshchenkoB <36041469+LeshchenkoB@users.noreply.github.com>
2019-10-25 08:48:42 +0700

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,62 @@
+jQuery(document).ready(function() {
+    jQuery('form').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated slideInUp',
+        offset: 50
+    });
+
+    jQuery('.headerAward').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated slideInUp',
+        offset: 50
+    });
+
+    jQuery('.awardItems').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated slideInUp',
+        offset: 50
+    });
+
+    jQuery('.awardBtn').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated slideInUp',
+        offset: 50
+    });
+
+    jQuery('.winterActivities').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated slideInUp',
+        offset: 50
+    });
+
+    jQuery('.headerDelux').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated slideInUp',
+        offset: 50
+    });
+
+    jQuery('.deluxeItems').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated slideInUp',
+        offset: 50
+    });
+
+    jQuery('.deluxBtn').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated slideInUp',
+        offset: 50
+    });
+
+    jQuery('.headerNews').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated slideInUp',
+        offset: 50
+    });
+
+    jQuery('.newsItems').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated slideInUp',
+        offset: 50
+    });
+
+    jQuery('.newsBtn').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated slideInUp',
+        offset: 50
+    });
+
+    jQuery('.footer').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated slideInUp',
+        offset: 50
+    });
+
+});
